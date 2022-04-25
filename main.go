4335267ecf80dff24eee906c2fcc300ee940e2c0
joanjanku2000 <janku.joan4@gmail.com>
2022-04-25 22:44:22 +0200

    Print,Println,Printf,Sprintf

--- main.go
+++ main.go
@@ -1,54 +1,31 @@
 package main
 
-import "fmt" // from go standard library:
+// from go standard library:
 // formatting strings and printing messages to the console
 // GO - strongly typed compiled lang , variable type cannot be changed
+import "fmt"
+
 
 func main(){
-	// Cannot compile code with unused vars or imports
-  var nameOne string = "joan" // var variableName type
+		
+	fmt.Print("Hello , ")
+fmt.Print("World  \n") // just like C and Java
 
-		var nameTwo = "Luigi" // infers the type
- 
-		var name3 string; // default empty string
+fmt.Println() // adds \n
 
-		fmt.Println("Name "+nameOne)
+fmt.Print("My age is ",22 , " \n")
 
-		fmt.Println("Second name "+nameTwo + " name 3 " + name3)
+age:= 22;  name:= "Joan";
 
-		name3 = nameOne;
-
-			fmt.Println("Now name3 is " + name3)
-
-			name4 := "yoshi" // not using var but using : , only done when initializing , only inside functions
-
-			
-				fmt.Println("Name4 is " + name4)
-
-				// numbers - integers floats
-
-				var age1 int = 22
-				var age2 = 30
-				age3 := 40
-
-				fmt.Println(age1 , age2 , age3)
-
-	 	//integer bit size (8,16,32,64) & memory
-
-			var numOne int8 = 25 // compile error if given number exceeds int bit number
-			var numTwo int64 = 45
-
-			// unsigned int 
-
-			var numThree uint = 12 //uint8 , uint16 , uint32 , uint64
-
-			fmt.Println(numOne , numTwo , numThree)
+fmt.Printf("My age is %v and my name is %v \n",age,name);
+fmt.Printf("My age is %q and my name is %q \n",age,name); // output quotes on strings
+fmt.Printf("My age is %v and of type is %T \n",age,age);
+fmt.Printf("You scored %0.2f points ! ",2.226) // limiting to 2 decimal points
 
 
-			//floats
+// Sprintf (save formatted strings)
 
-			var floatOne float32 = 1.4 // 32 or 64 even -2.3
-		 floatSixtyFour := 123.6
-		fmt.Println(floatOne ,floatSixtyFour)
+var str = fmt.Sprintf("My age is %v and my name is %v \n",age,name) // doesn't print it only returns it
+fmt.Print("\n The saved string is " ,str)
 
 }
